a3e4515ccd1c356b3be7fab4136239e738fdba08
rifat2113 <119580998+rifat2113@users.noreply.github.com>
2024-11-15 12:54:11 +0530

    Add files via upload

---
+++ azurerm_vm/main.tf
@@ -0,0 +1,391 @@
+locals {
+  cmk_enabled_virtual_machines = true
+  vm_data_disks = {}
+  key_permissions = ["Get", "Create", "List", "Restore", "Recover", "UnwrapKey", "WrapKey", "Purge", "Encrypt", "Decrypt", "Sign", "Verify"]
+  secret_permissions = ["Get"]
+}
+
+#---------------------------------------------------------------
+# Data Sources
+#---------------------------------------------------------------
+
+data "azurerm_client_config" "current" {}
+
+data "azurerm_resource_group" "rg" {
+  name = var.resource_group_name
+}
+
+data "azurerm_virtual_network" "vnet" {
+  name                = var.virtual_network_name
+  resource_group_name = data.azurerm_resource_group.rg.name
+}
+
+data "azurerm_subnet" "snet" {
+  name                 = var.subnet_name
+  virtual_network_name = data.azurerm_virtual_network.vnet.name
+  resource_group_name  = data.azurerm_resource_group.rg.name
+}
+
+data "azurerm_storage_account" "storeacc" {
+  name                = var.storage_account_name
+  resource_group_name = var.storage_rg
+}
+
+data "azurerm_key_vault" "kv" {
+  name                = var.key_vault_name
+  resource_group_name = var.key_vault_rg_name
+}
+
+#---------------------------------------------------------------
+# Generates SSH2 key Pair for Linux VM's (Dev Environment only)
+#---------------------------------------------------------------
+resource "tls_private_key" "rsa" {
+  count     = var.generate_admin_ssh_key ? 1 : 0
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+#----------------------------------------------------------
+# Key Vault Key & Random Password Resource
+#----------------------------------------------------------
+
+resource "azurerm_key_vault_key" "kv_key" {
+  name         = var.key_vault_key_name
+  key_vault_id = data.azurerm_key_vault.kv.id
+  key_type     = "RSA"
+  key_size     = 4096
+  key_opts     = ["decrypt", "encrypt", "sign", "unwrapKey", "verify", "wrapKey", ]
+}
+
+resource "random_password" "passwd" {
+  count       = (var.os_flavor == "linux" && var.disable_password_authentication == false && var.admin_password == null ? 1 : (var.os_flavor == "windows" && var.admin_password == null ? 1 : 0))
+  length      = var.random_password_length
+  min_upper   = 4
+  min_lower   = 2
+  min_numeric = 4
+  special     = false
+
+  keepers = {
+    admin_password = var.virtual_machine_name
+  }
+}
+
+#---------------------------------------
+# Network Interface for Virtual Machine
+#---------------------------------------
+resource "azurerm_network_interface" "nic" {
+  count                         = var.instances_count
+  name                          = var.instances_count == 1 ? lower("nic-${format("vm%s", lower(replace(var.virtual_machine_name, "/[[:^alnum:]]/", "")))}") : lower("nic-${format("vm%s%s", lower(replace(var.virtual_machine_name, "/[[:^alnum:]]/", "")), count.index + 1)}")
+  resource_group_name           = data.azurerm_resource_group.rg.name
+  location                      = data.azurerm_resource_group.rg.location
+  dns_servers                   = var.dns_servers
+  enable_ip_forwarding          = var.enable_ip_forwarding
+  enable_accelerated_networking = var.enable_accelerated_networking
+  internal_dns_name_label       = var.internal_dns_name_label
+  tags                          = var.tags
+
+  ip_configuration {
+    name                          = lower("ipconig-${format("vm%s%s", lower(replace(var.virtual_machine_name, "/[[:^alnum:]]/", "")), count.index + 1)}")
+    primary                       = true
+    subnet_id                     = data.azurerm_subnet.snet.id
+    private_ip_address_allocation = var.private_ip_address_allocation_type
+    private_ip_address            = var.private_ip_address_allocation_type == "Static" ? element(concat(var.private_ip_address, [""]), count.index) : null
+    # public_ip_address_id          = var.enable_public_ip_address == true ? element(concat(data.azurerm_public_ip.pip.*.id, [""]), count.index) : null
+  }
+}
+
+#----------------------------------------------------------------------------------------------------
+# Proximity placement group for virtual machines, virtual machine scale sets and availability sets.
+#----------------------------------------------------------------------------------------------------
+resource "azurerm_proximity_placement_group" "appgrp" {
+  count               = var.enable_proximity_placement_group ? 1 : 0
+  name                = lower("proxigrp-${var.virtual_machine_name}-${data.azurerm_resource_group.rg.location}")
+  resource_group_name = data.azurerm_resource_group.rg.name
+  location            = data.azurerm_resource_group.rg.location
+  tags                = var.tags
+
+}
+
+#-----------------------------------------------------
+# Manages an Availability Set for Virtual Machines.
+#-----------------------------------------------------
+resource "azurerm_availability_set" "aset" {
+  count                        = var.enable_vm_availability_set ? 1 : 0
+  name                         = lower("avail-${var.virtual_machine_name}-${data.azurerm_resource_group.rg.location}")
+  resource_group_name          = data.azurerm_resource_group.rg.name
+  location                     = data.azurerm_resource_group.rg.location
+  platform_fault_domain_count  = var.platform_fault_domain_count
+  platform_update_domain_count = var.platform_update_domain_count
+  proximity_placement_group_id = var.enable_proximity_placement_group ? azurerm_proximity_placement_group.appgrp.0.id : null
+  managed                      = true
+  tags                         = var.tags
+}
+
+#---------------------------------------
+# Linux Virutal machine
+#---------------------------------------
+resource "azurerm_linux_virtual_machine" "linux_vm" {
+  count                           = var.os_flavor == "linux" ? var.instances_count : 0
+  name                            = var.instances_count == 1 ? substr(var.virtual_machine_name, 0, 64) : substr(format("%s%s", lower(replace(var.virtual_machine_name, "/[[:^alnum:]]/", "")), count.index + 1), 0, 64)
+  resource_group_name             = data.azurerm_resource_group.rg.name
+  location                        = data.azurerm_resource_group.rg.location
+  size                            = var.virtual_machine_size
+  admin_username                  = var.admin_username
+  admin_password                  = var.disable_password_authentication == false && var.admin_password == null ? element(concat(random_password.passwd.*.result, [""]), 0) : var.admin_password
+  disable_password_authentication = var.disable_password_authentication
+  network_interface_ids           = [element(concat(azurerm_network_interface.nic.*.id, [""]), count.index)]
+  source_image_id                 = var.source_image_id != null ? var.source_image_id : null
+  provision_vm_agent              = true
+  allow_extension_operations      = true
+  dedicated_host_id               = var.dedicated_host_id
+  custom_data                     = var.custom_data != null ? var.custom_data : null
+  availability_set_id             = var.enable_vm_availability_set == true ? element(concat(azurerm_availability_set.aset.*.id, [""]), 0) : null
+  encryption_at_host_enabled      = var.enable_encryption_at_host
+  proximity_placement_group_id    = var.enable_proximity_placement_group ? azurerm_proximity_placement_group.appgrp.0.id : null
+  zone                            = var.vm_availability_zone
+  tags                            = var.tags
+
+  dynamic "admin_ssh_key" {
+    for_each = var.disable_password_authentication ? [1] : []
+    content {
+      username   = var.admin_username
+      public_key = var.admin_ssh_key_data == null ? tls_private_key.rsa[0].public_key_openssh : file(var.admin_ssh_key_data)
+    }
+  }
+
+  dynamic "source_image_reference" {
+    for_each = var.source_image_id != null ? [] : [1]
+    content {
+      publisher = var.custom_image != null ? var.custom_image["publisher"] : var.linux_distribution_list[lower(var.linux_distribution_name)]["publisher"]
+      offer     = var.custom_image != null ? var.custom_image["offer"] : var.linux_distribution_list[lower(var.linux_distribution_name)]["offer"]
+      sku       = var.custom_image != null ? var.custom_image["sku"] : var.linux_distribution_list[lower(var.linux_distribution_name)]["sku"]
+      version   = var.custom_image != null ? var.custom_image["version"] : var.linux_distribution_list[lower(var.linux_distribution_name)]["version"]
+    }
+  }
+
+  os_disk {
+    storage_account_type      = var.os_disk_storage_account_type
+    caching                   = var.os_disk_caching
+    disk_encryption_set_id    = var.disk_encryption_set_id != null? var.disk_encryption_set_id : azurerm_disk_encryption_set.des.id
+    disk_size_gb              = var.disk_size_gb
+    write_accelerator_enabled = var.enable_os_disk_write_accelerator
+    name                      = var.os_disk_name
+  }
+
+  additional_capabilities {
+    ultra_ssd_enabled = var.enable_ultra_ssd_data_disk_storage_support
+  }
+
+  dynamic "identity" {
+    for_each = var.managed_identity_type != null ? [1] : []
+    content {
+      type         = var.managed_identity_type
+      identity_ids = var.managed_identity_type == "UserAssigned" || var.managed_identity_type == "SystemAssigned, UserAssigned" ? var.managed_identity_ids : null
+    }
+  }
+
+  dynamic "boot_diagnostics" {
+    for_each = var.enable_boot_diagnostics ? [1] : []
+    content {
+      storage_account_uri = var.storage_account_name != null ? data.azurerm_storage_account.storeacc.primary_blob_endpoint : var.storage_account_uri
+    }
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+#---------------------------------------
+# Windows Virutal machine
+#---------------------------------------
+resource "azurerm_windows_virtual_machine" "win_vm" {
+  count                        = var.os_flavor == "windows" ? var.instances_count : 0
+  name                         = var.instances_count == 1 ? substr(var.virtual_machine_name, 0, 15) : substr(format("%s%s", lower(replace(var.virtual_machine_name, "/[[:^alnum:]]/", "")), count.index + 1), 0, 15)
+  computer_name                = var.instances_count == 1 ? substr(var.virtual_machine_name, 0, 15) : substr(format("%s%s", lower(replace(var.virtual_machine_name, "/[[:^alnum:]]/", "")), count.index + 1), 0, 15)
+  resource_group_name          = data.azurerm_resource_group.rg.name
+  location                     = data.azurerm_resource_group.rg.location
+  size                         = var.virtual_machine_size
+  admin_username               = var.admin_username
+  admin_password               = var.admin_password == null ? element(concat(random_password.passwd.*.result, [""]), 0) : var.admin_password
+  network_interface_ids        = [element(concat(azurerm_network_interface.nic.*.id, [""]), count.index)]
+  source_image_id              = var.source_image_id != null ? var.source_image_id : null
+  provision_vm_agent           = true
+  allow_extension_operations   = true
+  dedicated_host_id            = var.dedicated_host_id
+  custom_data                  = var.custom_data != null ? var.custom_data : null
+  enable_automatic_updates     = var.enable_automatic_updates
+  license_type                 = var.license_type
+  availability_set_id          = var.enable_vm_availability_set == true ? element(concat(azurerm_availability_set.aset.*.id, [""]), 0) : null
+  encryption_at_host_enabled   = var.enable_encryption_at_host
+  proximity_placement_group_id = var.enable_proximity_placement_group ? azurerm_proximity_placement_group.appgrp.0.id : null
+  patch_mode                   = var.patch_mode
+  zone                         = var.vm_availability_zone
+  timezone                     = var.vm_time_zone
+  tags                         = var.tags
+
+  dynamic "source_image_reference" {
+    for_each = var.source_image_id != null ? [] : [1]
+    content {
+      publisher = var.custom_image != null ? var.custom_image["publisher"] : var.windows_distribution_list[lower(var.windows_distribution_name)]["publisher"]
+      offer     = var.custom_image != null ? var.custom_image["offer"] : var.windows_distribution_list[lower(var.windows_distribution_name)]["offer"]
+      sku       = var.custom_image != null ? var.custom_image["sku"] : var.windows_distribution_list[lower(var.windows_distribution_name)]["sku"]
+      version   = var.custom_image != null ? var.custom_image["version"] : var.windows_distribution_list[lower(var.windows_distribution_name)]["version"]
+    }
+  }
+
+  os_disk {
+    storage_account_type      = var.os_disk_storage_account_type
+    caching                   = var.os_disk_caching
+    disk_encryption_set_id    = var.disk_encryption_set_id
+    disk_size_gb              = var.disk_size_gb
+    write_accelerator_enabled = var.enable_os_disk_write_accelerator
+    name                      = var.os_disk_name
+  }
+
+  additional_capabilities {
+    ultra_ssd_enabled = var.enable_ultra_ssd_data_disk_storage_support
+  }
+
+  dynamic "identity" {
+    for_each = var.managed_identity_type != null ? [1] : []
+    content {
+      type         = var.managed_identity_type
+      identity_ids = var.managed_identity_type == "UserAssigned" || var.managed_identity_type == "SystemAssigned, UserAssigned" ? var.managed_identity_ids : null
+    }
+  }
+
+  dynamic "winrm_listener" {
+    for_each = var.winrm_protocol != null ? [1] : []
+    content {
+      protocol        = var.winrm_protocol
+      certificate_url = var.winrm_protocol == "Https" ? var.key_vault_certificate_secret_url : null
+    }
+  }
+
+  dynamic "additional_unattend_content" {
+    for_each = var.additional_unattend_content != null ? [1] : []
+    content {
+      content = var.additional_unattend_content
+      setting = var.additional_unattend_content_setting
+    }
+  }
+
+  dynamic "boot_diagnostics" {
+    for_each = var.enable_boot_diagnostics ? [1] : []
+    content {
+      storage_account_uri = var.storage_account_name != null ? data.azurerm_storage_account.storeacc.0.primary_blob_endpoint : var.storage_account_uri
+    }
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+#-------------------------------------------------------------------------
+# Disk encryption set, Key Vault access policy, Key Vault role assignment
+#-------------------------------------------------------------------------
+
+resource "azurerm_disk_encryption_set" "des" {
+# count            = local.cmk_enabled_virtual_machines == true? var.des_count : 0
+  name                = "${var.disk_encryption_set_name}-des"
+  resource_group_name = data.azurerm_resource_group.rg.name
+  location            = data.azurerm_resource_group.rg.location
+  key_vault_key_id    = azurerm_key_vault_key.kv_key.id
+
+  identity {
+    type = "SystemAssigned"
+  }
+  tags = var.tags
+}
+
+resource "azurerm_key_vault_access_policy" "cmk" {
+  key_vault_id = data.azurerm_key_vault.kv.id
+  tenant_id = data.azurerm_client_config.current.tenant_id
+  object_id = azurerm_disk_encryption_set.des.identity.0.principal_id
+  key_permissions    = local.key_permissions
+  secret_permissions = local.secret_permissions
+}
+
+resource "azurerm_role_assignment" "kv_role" {
+  count                            = local.cmk_enabled_virtual_machines == true? var.des_count : 0
+  scope                            = data.azurerm_key_vault.kv.id
+  role_definition_name             = "Reader"
+  principal_id                     = var.disk_encryption_set_id != null? var.disk_encryption_set_id : azurerm_disk_encryption_set.des.identity.0.principal_id
+  skip_service_principal_aad_check = true
+}
+
+#---------------------------------------
+# Virtual machine data disks
+#---------------------------------------
+resource "azurerm_managed_disk" "data_disk" {
+  for_each             = local.vm_data_disks
+  name                 = "${var.virtual_machine_name}_DataDisk_${each.value.idx}"
+  resource_group_name  = data.azurerm_resource_group.rg.name
+  location             = data.azurerm_resource_group.rg.location
+  storage_account_type = lookup(each.value.data_disk, "storage_account_type", "StandardSSD_LRS")
+  create_option        = "Empty"
+  disk_size_gb         = each.value.data_disk.disk_size_gb
+  tags                 = merge({ "ResourceName" = "${var.virtual_machine_name}_DataDisk_${each.value.idx}" }, var.tags, )
+
+}
+
+resource "azurerm_virtual_machine_data_disk_attachment" "data_disk" {
+  for_each           = local.vm_data_disks
+  managed_disk_id    = azurerm_managed_disk.data_disk[each.key].id
+  virtual_machine_id = var.os_flavor == "windows" ? azurerm_windows_virtual_machine.win_vm[0].id : azurerm_linux_virtual_machine.linux_vm[0].id
+  lun                = each.value.idx
+  caching            = "ReadWrite"
+}
+
+
+#--------------------------------------------------------------
+# Azure Log Analytics Workspace Agent Installation for windows
+#--------------------------------------------------------------
+resource "azurerm_virtual_machine_extension" "omsagentwin" {
+  count                      = var.deploy_log_analytics_agent && var.log_analytics_workspace_id != null && var.os_flavor == "windows" ? var.instances_count : 0
+  name                       = var.instances_count == 1 ? "OmsAgentForWindows" : format("%s%s", "OmsAgentForWindows", count.index + 1)
+  virtual_machine_id         = azurerm_windows_virtual_machine.win_vm[count.index].id
+  publisher                  = "Microsoft.EnterpriseCloud.Monitoring"
+  type                       = "MicrosoftMonitoringAgent"
+  type_handler_version       = "1.0"
+  auto_upgrade_minor_version = true
+
+  settings = <<SETTINGS
+    {
+      "workspaceId": "${var.log_analytics_customer_id}"
+    }
+  SETTINGS
+
+  protected_settings = <<PROTECTED_SETTINGS
+    {
+    "workspaceKey": "${var.log_analytics_workspace_primary_shared_key}"
+    }
+  PROTECTED_SETTINGS
+}
+
+#--------------------------------------------------------------
+# Azure Log Analytics Workspace Agent Installation for Linux
+#--------------------------------------------------------------
+resource "azurerm_virtual_machine_extension" "omsagentlinux" {
+  count                      = var.deploy_log_analytics_agent && var.log_analytics_workspace_id != null && var.os_flavor == "linux" ? var.instances_count : 0
+  name                       = var.instances_count == 1 ? "OmsAgentForLinux" : format("%s%s", "OmsAgentForLinux", count.index + 1)
+  virtual_machine_id         = azurerm_linux_virtual_machine.linux_vm[count.index].id
+  publisher                  = "Microsoft.EnterpriseCloud.Monitoring"
+  type                       = "OmsAgentForLinux"
+  type_handler_version       = "1.13"
+  auto_upgrade_minor_version = true
+
+  settings = <<SETTINGS
+    {
+      "workspaceId": "${var.log_analytics_customer_id}"
+    }
+  SETTINGS
+
+  protected_settings = <<PROTECTED_SETTINGS
+    {
+    "workspaceKey": "${var.log_analytics_workspace_primary_shared_key}"
+    }
+  PROTECTED_SETTINGS
+}
