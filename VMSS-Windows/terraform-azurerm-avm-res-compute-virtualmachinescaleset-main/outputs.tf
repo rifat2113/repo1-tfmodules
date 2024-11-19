bc219e35d9776cbbbd8a96e236af7aed423dd98e
rifat2113 <119580998+rifat2113@users.noreply.github.com>
2024-11-19 15:48:41 +0530

    Add files via upload

---
+++ VMSS-Windows/terraform-azurerm-avm-res-compute-virtualmachinescaleset-main/outputs.tf
@@ -0,0 +1,14 @@
+output "resource" {
+  description = "All attributes of the Virtual Machine Scale Set resource."
+  value       = azurerm_orchestrated_virtual_machine_scale_set.virtual_machine_scale_set
+}
+
+output "resource_id" {
+  description = "The ID of the Virtual Machine Scale Set."
+  value       = azurerm_orchestrated_virtual_machine_scale_set.virtual_machine_scale_set.id
+}
+
+output "resource_name" {
+  description = "The name of the Virtual Machine Scale Set."
+  value       = azurerm_orchestrated_virtual_machine_scale_set.virtual_machine_scale_set.name
+}
---
+++ VMSS-Windows/terraform-azurerm-avm-res-compute-virtualmachinescaleset-main/terraform.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.116.0, < 4.0"
+    }
+    modtm = {
+      source  = "Azure/modtm"
+      version = "~> 0.3"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 3.6.2"
+    }
+  }
+}
+
